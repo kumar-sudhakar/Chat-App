54ca61064a8abd2ef1bb0ab8653e1cfb18dcb553
git config --global user.name "kumar-sudhakar <git config --global user.name "kumar-sudhakar" git config --global user.email "your-github-email@example.com>
2026-09-15 12:43:39 +0530

    feat(router): set up client routes and page components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import assets from './assets/assets' // Or whatever this file is named (e.g., './assets')
 
 const App = () => {
   return (
-    <div>App</div>
+   <div
+  className="min-h-screen w-full bg-cover bg-no-repeat bg-center"
+  style={{ backgroundImage: `url(${assets.bgImage})` }}
+>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
   )
 }
 
